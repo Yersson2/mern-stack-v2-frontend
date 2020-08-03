21a0eb317fe00dbe4531fc11e7488d1dafd0bc04
yersson <yersson2@gmail.com>
2020-08-03 12:20:53 -0500

    third  commit

--- src/components/CreateUser.jsx
+++ src/components/CreateUser.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import { useState } from 'react';
 
 const CreateUser = () => {
+
+  const[users, setUsers] = useState([]);
+
+  useEffect( () => {
+    (async () => {
+      const res = await axios.get('http://localhost:4000/api/users')
+      setUsers(res.data);
+      console.log(users);
+    }) ();
+  }, [users]);
+  
+ 
+
   return (
     <div>
       create user
